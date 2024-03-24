844480ad0b0e2d1e2c3dd7f17f9da5ffa0631473
Noah Hamid <noahhamid111@gmail.com>
2024-03-24 16:19:58 +0300

    nike_website

---
+++ tailwind_css/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0F9D58",
+      },
+      fontFamily: {
+        madim: "Mallana",
+        underdog: "Underdog",
+      },
+      screens: {},
+    },
+  },
+  plugins: [],
+};
